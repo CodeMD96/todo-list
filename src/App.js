749681ad347b1a179920539c62750aecb77738e5
CodeMD96 <codemd96@gmail.com>
2021-12-02 12:05:00 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useState } from "react"
+
 function App() {
+  const [list, setList] = useState(["do this", "do that"]);
+  const [input, setInput] = useState("");
+
+  const addHandler = () => {
+    let storedList =[...list];
+    input.replace(/ /g, "") &&
+      storedList.push(input);
+    setList(storedList);
+  };
+
+  const keypressHandler = (event) => {
+    if (event.which === 13) {
+      addHandler(input);
+    }
+  };
+
+  const removeHandler = (index) => {
+    let storedList = [...list];
+    storedList.splice(index, 1);
+    setList(storedList);
+  }
+
+  const changeHandler = (event) => {
+    setInput(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To do list</h1>
+      <input type="text" value={input} onChange={changeHandler} onKeyPress={keypressHandler} />
+      <button onClick={addHandler}>Add to list</button>
+      <div className="todo-list">
+        {list.map((item, index) => {
+                  return (
+                    <div key={index} className="list-item">
+                      <p>{item}</p>
+                      <button className="list-button" onClick={() => removeHandler(index)} >Done</button>
+                    </div>
+                  )
+              })}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
